db06fecb06eeb86647a00966bce47384d758f3fb
NathanMunsch <nathanmunschpro@gmail.com>
2023-12-24 22:50:53 +0100

    feat: Update default app config

--- TwitterLite.Server/Program.cs
+++ TwitterLite.Server/Program.cs
@@ -1,10 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using TwitterLite.Server.Data;
+using TwitterLite.Server.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors();
+builder.Services.AddDbContext<UserContext>(options =>
+{
+    options.UseSqlServer("Server=192.168.1.112;Initial Catalog=TwitterLite;User ID=nathan;Password=nathan;TrustServerCertificate=True;");
+});
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<JwtService>();
+
 builder.Services.AddControllers();
 
+
 var app = builder.Build();
+
+app.UseCors(options => options
+    .WithOrigins(new[] { "https://localhost:5173/" })
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+);
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
