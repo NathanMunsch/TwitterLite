4a5454d74730a2fac2faeb19eb7e8be63eeb7eb0
NathanMunsch <nathanmunschpro@gmail.com>
2023-12-24 23:39:39 +0100

    feat: Implement authentication api

---
+++ TwitterLite.Server/Controllers/AuthController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TwitterLite.Server.Data;
+using TwitterLite.Server.Dtos;
+using TwitterLite.Server.Helpers;
+using TwitterLite.Server.Models;
+
+namespace TwitterLite.Server.Controllers
+{
+    [Route("[Controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly UserRepository userRepository;
+        private readonly JwtService jwtService;
+
+        public AuthController(UserRepository userRepository, JwtService jwtService)
+        {
+            this.userRepository = userRepository;
+            this.jwtService = jwtService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterDto registerDto)
+        {
+            var user = new User
+            {
+                Username = registerDto.Username,
+                Password = BCrypt.Net.BCrypt.HashPassword(registerDto.Password),
+            };
+
+            if (user.Username.Length < 3) return BadRequest(new { message = "Username must contains at least 3 characters" });
+            if (userRepository.GetByUsername(user.Username) != null) return BadRequest(new { message = "Username already exists" });
+
+            var userCreated = userRepository.Create(user);
+
+            return Ok(new { message = "User created", userCreated });
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginDto loginDto)
+        {
+            var user = userRepository.GetByUsername(loginDto.Username);
+            if (user == null) return BadRequest(new { message = "Invalid credentials" });
+
+            if (!BCrypt.Net.BCrypt.Verify(loginDto.Password, user.Password)) return BadRequest(new { message = "Invalid credentials" });
+
+            var jwtToken = jwtService.Generate(user.Id);
+            Response.Cookies.Append("jwtToken", jwtToken, new CookieOptions
+            {
+                HttpOnly = true
+            });
+
+            return Ok(new { message = "Authentication successful" });
+        }
+
+        [HttpGet("getAuthenticatedUser")]
+        public IActionResult GetAuthenticatedUser()
+        {
+            try
+            {
+                var jwtToken = Request.Cookies["jwtToken"];
+                if (jwtToken == null) return BadRequest();
+
+                if (jwtService.IsTokenExpired(jwtToken)) return Unauthorized();
+
+                var userId = jwtService.GetUserIdFromToken(jwtToken);
+                var user = userRepository.GetById(userId);
+                if (user == null) return BadRequest();
+
+                return Ok(new { user });
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("jwtToken");
+
+            return Ok(new { message = "Logged out" });
+        }
+    }
+}
---
+++ TwitterLite.Server/Helpers/JwtService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace TwitterLite.Server.Helpers
+{
+    public class JwtService
+    {
+        private string secureKey = "tjis is a very secured key hjkdsqfqs ffqsdgb qklfgj";
+
+        public string Generate(int id)
+        {
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secureKey));
+            var credentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+            var jwtHeader = new JwtHeader(credentials);
+
+            var payload = new JwtPayload(id.ToString(), null, null, null, DateTime.Today.AddDays(1));
+            var securityToken = new JwtSecurityToken(jwtHeader, payload);
+
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+
+        private JwtSecurityToken GetJwtSecurityToken(string jwtToken)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secureKey);
+
+            tokenHandler.ValidateToken(jwtToken, new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+            }, out SecurityToken validatedToken);
+
+            return (JwtSecurityToken)validatedToken;
+        }
+
+        public bool IsTokenExpired(string jwtToken)
+        {
+            return (!(DateTime.Now < GetJwtSecurityToken(jwtToken).ValidTo.ToLocalTime()));
+        }
+
+        public int GetUserIdFromToken(string jwtToken)
+        {
+            return int.Parse(GetJwtSecurityToken(jwtToken).Issuer);
+        }
+    }
+}
