04175ea01e443e073f80773493665e92a9f56b66
NathanMunsch <nathanmunschpro@gmail.com>
2023-12-25 19:13:46 +0100

    feat: Add validation rules for username and password in api

--- TwitterLite.Server/Dtos/LoginDto.cs
+++ TwitterLite.Server/Dtos/LoginDto.cs
@@ -1,8 +1,15 @@
-﻿namespace TwitterLite.Server.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TwitterLite.Server.Dtos
 {
     public class LoginDto
     {
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(500, MinimumLength = 12)]
         public string Password { get; set; }
     }
 }
--- TwitterLite.Server/Dtos/RegisterDto.cs
+++ TwitterLite.Server/Dtos/RegisterDto.cs
@@ -1,8 +1,17 @@
-﻿namespace TwitterLite.Server.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TwitterLite.Server.Dtos
 {
     public class RegisterDto
     {
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        [RegularExpression(@"^[a-zA-Z0-9]+$")]
         public string Username { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#_])[A-Za-z\d@$!%*?&#_]{12,}$")]
         public string Password { get; set; }
     }
 }
