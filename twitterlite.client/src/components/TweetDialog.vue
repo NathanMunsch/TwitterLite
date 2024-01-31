29ed5da4779ec20bb3392c4c18bdea028b261404
Lightedd <hugophilippsext@gmail.Com>
2024-01-31 15:14:07 +0100

    updated Component Tweet

--- twitterlite.client/src/components/TweetDialog.vue
+++ twitterlite.client/src/components/TweetDialog.vue
@@ -1,16 +1,18 @@
 <script setup>
     import { ref } from 'vue';
-    
+
     var tweetContent = ref('');
 
     function sendTweet() {
     }
 </script>
 <template>
-    <v-card style="width: 50%;" class="mx-auto"     theme="dark" color="grey">
-        <v-card-text>Ecrivez un tweet...</v-card-text>
-        <v-textarea v-model="tweetContent" counter auto-grow bg-color="white" variant="solo" style="width: 75%; margin: auto;"></v-textarea>
-        <v-btn @click="sendTweet" color="blue" style="margin: 10px;">Envoyer</v-btn>
-    </v-card>
+    <v-dialog style="width: 50%;" class="mx-auto"     theme="dark" color="grey">
+        <v-card>
+            <v-card-text>Ecrivez un tweet...</v-card-text>
+            <v-textarea v-model="tweetContent" counter auto-grow bg-color="white" variant="solo" style="width: 75%; margin: auto;"></v-textarea>
+            <v-btn @click="sendTweet" color="blue" style="margin: 10px;">Envoyer</v-btn>
+        </v-card>
+    </v-dialog>
 </template>
 <style scoped></style>
