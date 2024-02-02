59b17114683f96a792b3ee2e430538f9879d4e60
NathanMunsch <nathanmunschpro@gmail.com>
2024-02-02 15:04:45 +0100

    refactor: Change validation regex in login / register dtos

--- TwitterLite.Server/Dtos/LoginDto.cs
+++ TwitterLite.Server/Dtos/LoginDto.cs
@@ -5,11 +5,9 @@ namespace TwitterLite.Server.Dtos
     public class LoginDto
     {
         [Required]
-        [StringLength(50, MinimumLength = 3)]
         public string Username { get; set; }
 
         [Required]
-        [MaxLength(500)]
         public string Password { get; set; }
     }
 }
--- TwitterLite.Server/Dtos/RegisterDto.cs
+++ TwitterLite.Server/Dtos/RegisterDto.cs
@@ -5,13 +5,20 @@ namespace TwitterLite.Server.Dtos
     public class RegisterDto
     {
         [Required]
-        [StringLength(50, MinimumLength = 3)]
+        [StringLength(32, MinimumLength = 3)]
         [RegularExpression(@"^[a-zA-Z0-9]+$")]
         public string Username { get; set; }
 
+
+        /*
+         * Require that at least one digit appear anywhere in the string
+         * Require that at least one lowercase letter appear anywhere in the string
+         * Require that at least one uppercase letter appear anywhere in the string
+         * Require that at least one special character appear anywhere in the string
+         * The password must be at least 12 characters long, but no more than 64
+        */
         [Required]
-        [MaxLength(500)]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#_])[A-Za-z\d@$!%*?&#_]{12,}$")]
+        [RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\]).{12,32}$")]
         public string Password { get; set; }
     }
 }
