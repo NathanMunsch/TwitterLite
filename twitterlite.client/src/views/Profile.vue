5fa00c13f30673beee6748d4f2811021c25c6f96
NathanMunsch <nathanmunschpro@gmail.com>
2024-04-12 10:45:03 +0200

    refactor(front): Change logo color

--- twitterlite.client/src/views/Profile.vue
+++ twitterlite.client/src/views/Profile.vue
@@ -17,7 +17,7 @@
 <template>
     <v-tooltip text="Logout">
         <template v-slot:activator="{ props }">
-            <v-btn style="position: fixed; top: 20px; right: 20px;" icon="mdi-exit-run" size="large" v-bind="props" @click="logout"></v-btn>
+            <v-btn style="position: fixed; top: 20px; right: 20px;" icon="mdi-exit-run" size="large" v-bind="props" @click="logout" color="red"></v-btn>
         </template>
     </v-tooltip>
 </template>
